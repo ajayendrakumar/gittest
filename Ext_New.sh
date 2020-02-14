cc420d26b56755fb9cbe40067c28a01492147e00
ajayendrakumar <samsofmailback@gmail.com>
2020-02-14 14:05:08 +0530

    some new line

--- Ext_New.sh
+++ Ext_New.sh
@@ -2,6 +2,6 @@
 for newextension
 do
     for files in $(ls $newextension/*.txt); do
-        echo $(date) $(mv -v $files ${files%.txt}.sh)
+        echo $(date) $(mv -v $files ${files%.txt}.new)
     done
 done
